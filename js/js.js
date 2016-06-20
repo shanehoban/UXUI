50a04314d331ed6b1c64b5ba0589180d5f5d085a
Dave <davidvalentineuiuxtraining2016@gmail.com>
2016-06-20 14:49:00 +0100

    add getting of location from url, refactor

--- js/js.js
+++ js/js.js
@@ -1,6 +1,5 @@
-
-
 var API_URL = 'http://127.0.0.1:1337/';
+
 var ORDER = [];
 
 var MENU;
@@ -137,40 +136,16 @@ var populateMenu = function(menu){
 	setupMenuListeners();
 }
 
+var getLocation = function(){
+	var url = window.location.href;
+    var regex = new RegExp("[?&]loc(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    console.log("Location", decodeURIComponent(results[2].replace(/\+/g, " ")));
+}
 
-var populateLocations = function(locations){
-	console.log('Locations', locations);
-	var locationHTML = '';
-	for(var i = 0; i < locations.length; i++){
-		locationHTML += '<li><a class="block-link" href="menu.html?loc=' + encodeURI(locations[i]) + '">';
-		locationHTML += locations[i];
-		locationHTML += '<i class="fa fa-chevron-right"></i></a></li>';
-	}
-	$('.location-list').html(locationHTML);
-};
-
-var getResterauntList = function(){
-	if(self.fetch){
-		fetch(API_URL + 'menus').then(function(data){
-			return data.json();
-		}).then(function(data){
-			console.log(data);
-			populateLocations(data.locations);
-		});
-	} else {
-	// ajax fallback
-		$.ajax({
-			url: API_URL + 'menus',
-			method: 'GET',
-			success: function(data){
-				data = JSON.parse(data);
-				populateLocations(data.locations);
-			},
-			error: function(err){
-				console.log(err);
-			}
-		});
-	}
-};
-
+$(document).ready(function(){
+	getLocation();
+});
 
