6fe1c2f8fb31c5c7bf5fb37774226d70a1cf09ce
Dave <davidvalentineuiuxtraining2016@gmail.com>
2016-06-22 13:05:44 +0100

    rename js.js, add api url to file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var API_URL =  API_URL || 'http://127.0.0.1:1337/' ;
+
 // Array Remove - By John Resig (MIT Licensed)
 Array.prototype.remove = function(from, to) {
   var rest = this.slice((to || from) + 1 || this.length);
